4b8c3b47e5f51e5a268d4f82134185a857621129
W.J.G <wjgaas@126.com>
2023-12-06 10:52:59 +0800

    Update download.py

--- download.py
+++ download.py
@@ -4,8 +4,20 @@
 
 # Or an entire repository
 from huggingface_hub import snapshot_download
-snapshot_download("playgroundai/playground-v2-1024px-aesthetic", ignore_patterns=["diffusion_pytorch_model.safetensors","diffusion_pytorch_model.bin","diffusion_pytorch_model.fp16.bin","model.safetensors","pytorch_model.fp16.bin","pytorch_model.bin"])
-
+from huggingface_hub import snapshot_download
+snapshot_download("playgroundai/playground-v2-1024px-aesthetic",
+                  ignore_patterns=["unet/diffusion_pytorch_model.safetensors",
+                                   "unet/diffusion_pytorch_model.bin",
+                                   "unet/diffusion_pytorch_model.fp16.bin",
+                                   "vae/diffusion_pytorch_model.safetensors",
+                                   "vae/diffusion_pytorch_model.bin",
+                                   "vae/diffusion_pytorch_model.fp16.bin",
+                                   "text_encoder/text_encoder_2model.safetensors",
+                                   "text_encoder/pytorch_model.fp16.bin",
+                                   "text_encoder/pytorch_model.bin",
+                                   "text_encoder_2/text_encoder_2model.safetensors",
+                                   "text_encoder_2/pytorch_model.fp16.bin",
+                                   "text_encoder_2/pytorch_model.bin"])
 # Download from a dataset
 #from huggingface_hub import snapshot_download
 # snapshot_download(repo_id="bigcode/starcoderdata", repo_type="dataset", allow_patterns=["git-commits-cleaned/train-00000-of-00055.parquet", "github-issues-filtered-structured/train-00000-of-00059.parquet"])
